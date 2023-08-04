5a31ac252a63538ebc57fad2884e1a86b6915d98
vinit-chauhan <chauhanvinit23@gmail.com>
2023-08-04 15:55:03 -0400

    Moved creds to .env file

--- main.py
+++ main.py
@@ -5,24 +5,39 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 # Gmail account details
-sender_email = ""
-password = ""
+sender_email = ''
+password = ''
+message: MIMEMultipart
+server: smtplib.SMTP
 
-# Create a message
-message = MIMEMultipart()
-message["From"] = sender_email
-message["Subject"] = "Applying for a part-time job"
 
-global server
-try:
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.login(sender_email, password)
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+def read_creds():
+    global sender_email, password
+    creds = []
+
+    with open('.env', 'r') as env:
+        for line in env:
+            creds.append(line.split('=')[1][:-1])
+    return creds
+
+def init():
+    global message, server
+    # Create a message
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["Subject"] = "Applying for a part-time job"
+
+    try:
+        server = smtplib.SMTP("smtp.gmail.com", 587)
+        server.starttls()
+        creds = read_creds()
+        server.login(creds[0], creds[1])
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
 
 
 def send_scheduled_email_to(receiver_email, body_text_file, attachment_file):
+    global message
     print('Attempting to send mail to', receiver_email)
 
     message["To"] = receiver_email
@@ -35,25 +50,33 @@ def send_scheduled_email_to(receiver_email, body_text_file, attachment_file):
         part = MIMEBase("application", "octet-stream")
         part.set_payload(file.read())
         encoders.encode_base64(part)
-        part.add_header("Content-Disposition", f"attachment; filename=resume.pdf")
+        part.add_header("Content-Disposition", "attachment; filename=resume.pdf")
 
         message.attach(part)
 
     # Send the email
     try:
+        global server
         server.sendmail(sender_email, receiver_email, message.as_string())
         print("Email sent successfully.")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
 
+def done():
+    global server
+    server.quit()
+
+
 # Schedule the email to be sent every day at a specific time (24-hour format)
 # schedule.every().day.at("08:00").do(send_scheduled_email)
 
 if __name__ == '__main__':
-    receiver = ""
-    send_scheduled_email_to(receiver, "res/body/company-1.txt", "res/resumes/resume1.pdf")
-    server.quit()
+    init()
+
+    send_scheduled_email_to("ADD RECEIVERS HERE", "res/body/company-1.txt", "res/resumes/resume1.pdf")
+
+    done()
 
 # while True:
 #     schedule.run_pending()
