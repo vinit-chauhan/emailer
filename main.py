fc83d3a317024080ec2a15f216786995b8b87ab3
vinit-chauhan <chauhanvinit23@gmail.com>
2024-04-24 20:22:53 -0400

    separate list file

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import configparser
 
+from send_list import send_list
+
 
 def read_config():
     config = configparser.ConfigParser()
@@ -46,17 +48,12 @@ def send_scheduled_email_to(server: smtplib.SMTP, sender_email, receivers, body_
 if __name__ == '__main__':
     sender_email, password = read_config()
 
-    list = [
-        {'receiver_email': [""], 'body_file': 'some.txt',
-            'attachment_file': 'some.pdf', 'reference': 'Some ref or remove it'},
-    ]
-
     try:
         with smtplib.SMTP("smtp.gmail.com", 587) as server:
             server.starttls()
             server.login(sender_email, password)
 
-            for item in list:
+            for item in send_list:
                 send_scheduled_email_to(server, sender_email, item['receiver_email'], f"res/body/{item['body_file']}",
                                         f"res/resumes/{item['attachment_file']}", item.get('reference', None))
             server.quit()
---
+++ send_list.py
@@ -0,0 +1,4 @@
+send_list = [
+    {'receiver_email': [""], 'body_file': 'some.txt',
+        'attachment_file': 'some.pdf', 'reference': 'Some ref or remove it'},
+]
