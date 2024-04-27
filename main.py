8e4ec9b2d09b63da669bbf4584fec2d8cc05247b
vinit-chauhan <chauhanvinit23@gmail.com>
2024-04-27 13:50:33 -0400

    Made it a web service

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import configparser
-from typing import List
+from fastapi import FastAPI, HTTPException
 
 from send_list import send_list, test_list
+
+app = FastAPI()
 
 
 def read_config():
@@ -23,7 +25,7 @@ def read_file(file_path):
 subject = "Application for a Part-time Job"
 
 
-def send_scheduled_email_to(server: smtplib.SMTP, sender_email: str, receivers: List[str], body_text_file: str, attachment_file: str, reference=None):
+def send_scheduled_email_to(server: smtplib.SMTP, sender_email: str, receivers: list[str], body_text_file: str, attachment_file: str, reference=None):
 
     for receiver_email in receivers:
         message = MIMEMultipart()
@@ -42,26 +44,48 @@ def send_scheduled_email_to(server: smtplib.SMTP, sender_email: str, receivers: 
                             "attachment; filename=resume.pdf")
             message.attach(part)
 
-        server.sendmail(sender_email, receiver_email, message.as_string())
-        print(f"Email sent to {receiver_email} successfully.")
+        # server.sendmail(sender_email, receiver_email, message.as_string())
+
+    return receivers
 
 
-if __name__ == '__main__':
-    global is_test
-    is_test = True
+global is_test
+is_test = True
 
-    sender_email, password = read_config()
+sender_email, password = read_config()
+send_list = send_list if not is_test else test_list
 
-    send_list = send_list if not is_test else test_list
+server: smtplib.SMTP = None
 
+try:
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(sender_email, password)
+
+except Exception as e:
+    raise (f"An error occurred: {str(e)}")
+
+
+@app.get('/send')
+def send():
+    sent_to = []
     try:
-        with smtplib.SMTP("smtp.gmail.com", 587) as server:
-            server.starttls()
-            server.login(sender_email, password)
+        for item in send_list:
+            sent_to.extend(send_scheduled_email_to(server, sender_email, item['receiver_email'], f"res/body/{item['body_file']}",
+                                                   f"res/resumes/{item['attachment_file']}", item.get('reference', None)))
 
-            for item in send_list:
-                send_scheduled_email_to(server, sender_email, item['receiver_email'], f"res/body/{item['body_file']}",
-                                        f"res/resumes/{item['attachment_file']}", item.get('reference', None))
-            server.quit()
+        return {"Status": "Success", "sent_to": sent_to}
+
     except Exception as e:
-        print(f"An error occurred: {str(e)}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+@app.get('/shutdown')
+def shutdown():
+    server.quit()
+    exit(0)
+
+
+@app.get('/show-list')
+def show_list():
+    return send_list
